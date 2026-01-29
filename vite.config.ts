200257b33bea498ccd45718dbcab7ad7cc669433
赤壁誠司 <akakabe_s@up-cloud.com>
2026-01-29 10:57:09 +0900

    Install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import ViteRails from "vite-plugin-rails";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     ViteRails(),
   ],
 })
